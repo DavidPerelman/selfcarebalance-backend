518d8f2029ca247ef425a0ebe8c4f5f1ebf5b03c
DavidPerelman <53565536+DavidPerelman@users.noreply.github.com>
2025-05-26 18:52:54 +0300

    cleanup

--- app/main.py
+++ app/main.py
@@ -5,33 +5,22 @@ from app.core.dependencies import get_current_user
 from app.models.user import User
 from app.routes import mood, auth_debug
 import os
-from beanie import init_beanie
 import dotenv
-from fastapi import FastAPI
-from motor.motor_asyncio import AsyncIOMotorClient
-
-from .models.user import User
 
 dotenv.load_dotenv()
 
 DB_URL = os.getenv("MONGODB_URL")
 
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    await init_db()  # 🟢 כאן נבצע אתחול למסד הנתונים
-    yield  # המשך הרצת האפליקציה
-    # (כאן תוכל לשים קוד לכיבוי אם תרצה בעתיד)
+    await init_db()
+    yield
 
 
 app = FastAPI(lifespan=lifespan)
 
 router = APIRouter()
-
-
-@app.on_event("startup")
-async def connect_to_db():
-    client = AsyncIOMotorClient(DB_URL)
-    await init_beanie(client.get_database(), document_models=[User])
 
 
 @app.get("/")
